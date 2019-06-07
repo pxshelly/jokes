23d2f92b7ff54a3dd0f49da9bfb29131526fc125
Shelly <pxshelly@gmail.com>
2019-06-07 12:16:58 -0700

    Add functionality to fetch joke from external API

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      joke: ''
+    }
+  }
+
+  getJoke() {
+    axios.get('https://jokes-api.herokuapp.com/api/joke')
+      .then(response => {
+        console.log(response);
+        this.setState({joke: response.data.value.joke})
+      })
+      .catch(error => console.log(error))
+  }
+
+  componentDidMount() {
+    this.getJoke();
   }
 
   render() {
     return (
-      <div>Hello</div>
+      <div>{this.state.joke}</div>
     )
   }
 }
